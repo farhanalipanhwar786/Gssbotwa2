d15319b9c1acaecc2deaf0f22b0b7d90038964e4
farhanalipanhwar786 <163797394+farhanalipanhwar786@users.noreply.github.com>
2024-04-03 00:42:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
+global.ownername = ' 𝐵𝛨𝛥𝛪';
 global.owner = ['917050906659'];
 global.premium = ['917050906659'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.author = "=  𝚩𝚯𝚻𝐖𝚫';
 global.sessionName = 'session';
 global.prefa = ['', '!', '.'];
 global.sp = '✪';
